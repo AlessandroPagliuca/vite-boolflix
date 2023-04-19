e97422198fad204591d9db3f645b31f6683879fd
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-19 14:51:27 +0200

    Upgraded HeaderComp

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-
-  </div>
+    <HeaderComp/>
 </template>
 
 <script>
+import HeaderComp from './components/HeaderComp.vue';
   export default {
+    name: 'App',
+    components:{
+      HeaderComp,
+    }
     
   }
 </script>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="p-3">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <h1 class="text-uppercase text-danger">netflix</h1>
+            <SearchBar/>
+        </div>
+    </header>
+</template>
+
+<script>
+    import SearchBar from './SearchBar.vue';
+    export default {
+        name: 'HeaderComp',
+        components:{
+            SearchBar,
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    header{
+        width: 100%;
+        height: 80px;
+        background-color: rgb(5, 5, 5,);
+        
+    }
+
+</style>
