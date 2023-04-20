53bf8b87322647767f4a2c085b1f862708e66aa8
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-20 18:49:37 +0200

    Upgraded SingleCard and Added in MainComp

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -9,20 +9,9 @@
 
                 <div class="col-8 col-sm-6 col-md-4 col-lg-3 p-3" v-for="movie in store.moviesList">
                     
-                    <div class="card">
-
-                        <img class="card-img-top" :src="store.imgBaseURL + movie.poster_path" :alt="movie.title">
-                        
-                        <div class="card-body">
-
-                            <p class="fw-semibold">Title: {{ movie.title }}</p>
-                            <p class="fw-semibold">Original title: {{ movie.original_title }}</p>
-                            <p class="fw-semibold">Original language: {{ movie.original_language }}</p>
-                            <p class="fw-semibold">Vote: {{ movie.vote_average }}</p>
-
-                        </div>
-
-                    </div>
+                    <SingleCard :title="movie.title" :src_image="store.imgBaseURL + movie.poster_path"
+                    :original_title="movie.original_title" :original_language="movie.original_language"
+                    :vote_average="movie.vote_average"/>
 
                 </div>
 
@@ -34,25 +23,13 @@
            
             <h2 class="text-white p-3">Tv / Series</h2>
 
-
             <div class="row justify-content-center">
 
                 <div class="col-8 col-sm-6 col-md-4 col-lg-3 p-3" v-for="serie in store.seriesList">
-                    
-                    <div class="card">
 
-                        <img class="card-img-top" :src="store.imgBaseURL + serie.poster_path" :alt="serie.title">
-                        
-                        <div class="card-body">
-
-                            <p class="fw-semibold">Title: {{ serie.name }}</p>
-                            <p class="fw-semibold">Original title: {{ serie.original_title }}</p>
-                            <p class="fw-semibold">Original language: {{ serie.original_language }}</p>
-                            <p class="fw-semibold">Vote: {{ serie.vote_average }}</p>
-
-                        </div>
-
-                    </div>
+                    <SingleCard :title="serie.name" :src_image="store.imgBaseURL + serie.poster_path"
+                    :original_title="serie.original_title" :original_language="serie.original_language"
+                    :vote_average="serie.vote_average"/>
 
                 </div>
 
@@ -65,8 +42,12 @@
 
 <script>
     import {store} from '../data/store';
+    import SingleCard from './SingleCard.vue';
     export default {
         name: 'MainComp',
+        components:{
+            SingleCard,
+        },
         data(){
             return{
                 store,
@@ -84,29 +65,6 @@
         height: calc(100vh - 80px);
         background: #2b2727;
         overflow-y: auto;
-
-        .card{
-            position: relative;
-            border: 2px solid black;
-            .card-body{
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                opacity: 0;
-                transition: 0.7s;
-            }
-
-            &.card:hover .card-body{
-                opacity: 1;
-                background: rgba(20, 20, 20, 0.5);
-                color: white;
-            }
-
-        }
-        
-
         
     }
 
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="card">
+
+        <img class="card-img-top" :src="src_image" :alt="title">
+
+        <div class="card-body">
+
+            <p class="fw-semibold">Title: {{ title }}</p>
+            <p class="fw-semibold">Original title: {{ original_title }}</p>
+            <p class="fw-semibold">Original language: {{ original_language }}</p>
+            <p class="fw-semibold">Vote: {{ vote_average }}</p>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SingleCard',
+    props: [
+        'title',
+        'src_image',
+        'original_title',
+        'original_language',
+        'vote_average',
+    ],
+
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    position: relative;
+    border: 2px solid black;
+    height: 600px;
+
+    img{
+        width: 100%;
+        height: 100%;
+    }
+    .card-body {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        opacity: 0;
+        transition: 0.7s;
+    }
+
+    &.card:hover .card-body {
+        opacity: 1;
+        background: rgba(20, 20, 20, 0.5);
+        color: white;
+    }
+
+}
+</style>
